e7644898ab579971698c6969f6468b1060bb74a6
falarion08 <barrion.ryanonil@gmail.com>
2023-12-07 10:25:03 +0800

    Implemented Pattern Matching

--- main.m
+++ main.m
@@ -51,7 +51,111 @@ end
 
 segmentedChars = charLevelSegmentation(binarizedImage,colVals,colSize);
 
-imshow(segmentedChars{1});
+templateChars = preprocessTemplate();
+
+temp1 = segmentedChars{1};
+imDim = size(temp1);
+temp2 = imresize(templateChars{36},imDim);
+
+% display(corr2(temp1,temp2));
+
+% imshow(temp2);
+licensePlateNumber = matchChars(segmentedChars,templateChars);
+display(licensePlateNumber)
+function licensePlateNumber = matchChars(licensPlateChars, templateChars)
+    licensePlateNumber = "";
+    uniqueChars = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9'];
+
+    for i =1:6
+        currChar = licensPlateChars{i}; 
+        currCharDim = size(currChar); 
+
+        maxCorr = 0.0;
+        maxCorrIndex = 1;
+
+        for j = 1:36
+            currTemplate = templateChars{j};
+            currTemplate = imresize(currTemplate,currCharDim);
+            
+            r = corr2(currChar,currTemplate);
+
+            if(r>maxCorr && r >=0.50)
+                maxCorr = r;
+                maxCorrIndex = j;
+            end
+        end
+        licensePlateNumber =strcat(licensePlateNumber,uniqueChars(maxCorrIndex));
+    end
+end 
+
+
+function templateChars = preprocessTemplate()
+    uniqueChars = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9'];
+    totalUnique = 36; 
+    strPrefix = 'LicensePlateBinary/';
+    strSuffix = '_template.bmp';
+    templateChars = {};
+
+for img_index =1:totalUnique
+    img = imread(strcat(strPrefix,uniqueChars(img_index),strSuffix));
+    img = flipColors(img);
+
+    imDim = size(img); 
+    rowSize = imDim(1);
+    colSize = imDim(2); 
+
+    rowVals = zeros(1,rowSize);
+    colVals = zeros(1,colSize);
+
+    for i = 1:rowSize
+        rowVals(i) = sum(img(i,:))./255;
+    end
+    
+    for i = 1:colSize
+        colVals(i) = sum(img(:,i))./255;
+    end
+    
+    lastWhite = 0; 
+    for i = 1:colSize
+        if(i < colSize && colVals(i+1) ~= 0 && colVals(i) == 0)
+            lastWhite = i;
+        elseif(colVals(i) == 0 && lastWhite ~= 0)
+            img = img(:,lastWhite:i);
+            lastWhite = 0;
+        end
+    end
+    lastWhite = 0;
+
+    for i = 1:rowSize
+        if(i < rowSize && rowVals(i+1) ~= 0 && rowVals(i) == 0)
+            lastWhite = i;
+        elseif(rowVals(i) == 0 && lastWhite ~= 0)
+            img = img(lastWhite:i,:);
+            lastWhite = 0;
+        end
+    end
+    
+    templateChars = [templateChars, img];
+end
+
+
+end
+
+function binarizedImage = flipColors(binarizedImage)
+    imDim = size(binarizedImage); 
+    rowSize = imDim(1);
+    colSize = imDim(2); 
+
+    for i=1:rowSize
+        for j=1:colSize
+            if(binarizedImage(i,j) == 255)
+                binarizedImage(i,j) = 0; 
+            else
+                binarizedImage(i,j) = 255; 
+            end
+        end
+    end
+end
 
 function segmentedChars = charLevelSegmentation(binarizedImage,colVals,colSize)
     segmentedChars = {};
